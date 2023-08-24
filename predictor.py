21cd1fa3e4ccf76ff07a192fd7967c391721ddd3
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-23 20:01:38 -0400

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -6,17 +6,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-def get_stock_data(ticker, start_date, end_date):
+def get_stock_data(ticker, s, e):
     stock_data = yf.Ticker(ticker)
-    return stock_data.history(period='1d', start=start_date, end=end_date)
+    return stock_data.history(period='1d', start=s, end=e)
 
-def preprocess_data(data, window_size):
+def preprocess_data(data, size):
     X, y = [], []
-    for i in range(len(data) - window_size - 1):
-        X.append(data['Close'].iloc[i:i + window_size].values)
-        y.append(data['Close'].iloc[i + window_size])
+    for i in range(len(data) - size - 1):
+        X.append(data['Close'].iloc[i:i + size].values)
+        y.append(data['Close'].iloc[i + size])
     return np.array(X), np.array(y)
 
+# ticker = apple
 ticker = 'AAPL'
 start_date = '2020-01-01'
 end_date = '2021-01-01'
@@ -24,14 +25,16 @@ end_date = '2021-01-01'
 data = get_stock_data(ticker, start_date, end_date)
 X, y = preprocess_data(data, window_size=5)
 
+# preprocessing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+# storing model
 predictions = model.predict(X_test)
-print(f'Mean Squared Error: {mean_squared_error(y_test, predictions)}')
 
+# plotting data
 plt.plot(y_test, label="True prices")
 plt.plot(predictions, label="Predicted prices")
 plt.legend()
